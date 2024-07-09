cac2a7cfb78597b78ffd7f012d70c14d1c49d2d1
unknown <yaldajafari383@gmail.com>
2024-07-09 17:22:00 +0430

    Fix LogIn

--- registeration.h
+++ registeration.h
@@ -30,7 +30,7 @@ public:
     void showPasswordState(UserInfo::state state);
     void adjustWidgetPositions(int offset);
 private slots:
-    void on_pushButton_3_clicked();
+    void on_pushButton_4_clicked();
 private:
     Ui::registeration *ui;
     UserInfo *user;
