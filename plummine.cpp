f0bb47ca9e321413b059391ae97214d72c862ba6
unknown <yaldajafari383@gmail.com>
2024-07-10 21:21:12 +0430

    Add Timer

--- plummine.cpp
+++ plummine.cpp
@@ -9,17 +9,25 @@ PlumMine::PlumMine(QPointF pos  , QGraphicsScene * _scene,home *adrs) :
     connect(changetimer, &QTimer::timeout, this, &PlumMine::HandlePlumMine);
     changetimer->start(500);
 
+    show();
+    QTimer* deleteTimer = new QTimer(this);
+    connect(deleteTimer, &QTimer::timeout, this, &PlumMine::deleteObject);
+    deleteTimer->start(2000);
+}
+
+void PlumMine::HandlePlumMine()
+{
+
     qreal cellWidth = 77;
     qreal cellHeight = 70;
     qreal startX= 119 + (this->position.first-2)*77 , startY = 36 + (this->position.second-2)*70;
 
-    if(pos.y()>=105 && pos.y()<178){
+    if(pos().y()>=105 && pos().y()<178){
         startY -= 4;
     }
-    else if(pos.y()>=398){
+    else if(pos().y()>=398){
         startY -= 4;
     }
-    //should check
     QList<QGraphicsItem*> itemList = scene->items();
     for (QGraphicsItem* item : itemList) {
 
@@ -35,17 +43,9 @@ PlumMine::PlumMine(QPointF pos  , QGraphicsScene * _scene,home *adrs) :
                 }
             }
         }
-    }
-    show();
-    QTimer* deleteTimer = new QTimer(this);
-    connect(deleteTimer, &QTimer::timeout, this, &PlumMine::deleteObject);
-    deleteTimer->start(2000);
-}
-
-void PlumMine::HandlePlumMine()
-{
     setPixmap(QPixmap(":/new/prefix1/Layer 2.png"));
     setScale(0.1);
+    }
 }
 
 void PlumMine::deleteObject() {
